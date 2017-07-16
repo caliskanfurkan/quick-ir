bfd8ca5ef37634cccb6003251e9096a4116c0bae
Furkan ÇALIŞKAN <caliskanfurkan@gmail.com>
2017-07-16 15:42:15 +0300

    initial
    initial

---
+++ main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import wmi
+c = wmi.WMI("HOSTNAME",user="HOSTNAMEORDOMAINNAME\USER",password="PASSWORD")
+print "\n\nOperating System"
+print "--------------------------------"
+for os in c.Win32_OperatingSystem():
+  print os.Caption
+  
+print "\n\nStartup Items"
+print "--------------------------------"
+for s in c.Win32_StartupCommand():
+  print "[%s] %s <%s>" % (s.Location, s.Caption, s.Command)
+
+print "\n\nNetwork shares"
+print "------------------------"
+
+for share in c.Win32_Share():
+  print share.Name, share.Path
+
+print "\n\nRunning processes"
+print "---------------------"
+
+for process in c.Win32_Process():
+  print process.ProcessId, process.Name
